7963ed2c8f22168026906a8994a3fb8241dae200
barba <barba@LAPTOP-AQMH51OP>
2023-03-16 12:09:22 +0000

    versao inicial

---
+++ Eq2Garu/Eq2Garu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eq2Garu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
