271a23f3185de9fcd894eeb6cb36882ea39544fe
barba <barba@LAPTOP-AQMH51OP>
2023-03-23 13:04:26 +0000

    Cálculo efetuado

--- Eq2Garu/Eq2Garu/Controllers/HomeController.cs
+++ Eq2Garu/Eq2Garu/Controllers/HomeController.cs
@@ -13,10 +13,49 @@ namespace Eq2Garu.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+
+        public IActionResult Index(int A, int B, int C)
         {
+            //vars auxiliares
+            double delta = B * B - 4 * A * C;
+            string x1 = "", x2 = "";
+
+
+            // posso calcular as raízes?
+            if (A != 0)
+            {
+                //há raizes reais?
+                if (delta >= 0)
+                {
+                    // sim, há
+                    x1 = (-B + Math.Sqrt(delta)) / (2 * A) + "";
+                    x2 = (-B - Math.Sqrt(delta)) / (2 * A) + "";
+
+
+
+                }
+                else
+                {
+
+                    //não há raizes reais
+                    //x1=-b/2/a + sqrt(abs(-delta))/2/a i
+                    //x2 = -b / 2 / a - sqrt(abs(-delta)) / 2 / a i
+
+                    x1 = -B / 2 / A + " + " + Math.Sqrt(-delta) / 2 / A + " i ";
+                    x2 = -B / 2 / A + " - " + Math.Sqrt(-delta) / 2 / A + " i ";
+
+                }
+            }
+            //Preparar os dados para serem enviados para a View
+            ViewBag.X1 = x1;
+            ViewBag.X2 = x2;
+
+
             return View();
         }
+
+
+
 
         public IActionResult Privacy()
         {
